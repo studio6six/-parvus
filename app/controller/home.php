d6728c2ca13a279d50989f1a541e0977b95ec2cd
Andre.st-jacques <andre.st-jacques@studio6six.com>
2024-03-26 18:18:00 -0400

    core change

--- app/controller/home.php
+++ app/controller/home.php
@@ -1,26 +1,29 @@
 <?php
-
-/**
- * Class Home
- *
- * Please note:
- * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
- * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
- *
- */
 class Home extends Controller
 {
-    /**
-     * PAGE: index
-     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
-     */
-    public function index()
+    public function __construct(){
+		parent::__construct();
+	}
+
+    public function index($request = null)
     {
+        $data['title'] = 'Welcome';
+        // load views
+		$this->view->rendertemplate('header',$data);
+		//$this->view->render('home/index',$data);
+		//$this->view->rendertemplate('footer',$data);
+    }
+
+    /*public function index($request = null)
+    {
+        $data['title'] = 'Welcome';
         // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/index.php';
         require APP . 'view/_templates/footer.php';
-    }
+    }*/
+
+
 
     /**
      * PAGE: exampleone
